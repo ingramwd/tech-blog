bdf242c90d0da11f4a9eac111fa76b27b8047b48
ingramwd <Ingramwd01@gmail.com>
2022-02-12 22:17:03 -0500

    set up server and connection to DB and set up .ENV

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const routes = require('./controllers');
+const path = require('path');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//turns on connection to the DB and SERVER
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
